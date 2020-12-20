8c86e21cc5e69aa12130e535899fb05d48346589
Najmus Sahar <najmus.sahar77@gmail.com>
2020-12-20 16:25:39 +0530

    tokenbasedauthentication

---
+++ TokenAuthenticationInWebAPI/TokenAuthenticationInWebAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using TokenAuthenticationInWebAPI.Models;
+
+namespace TokenAuthenticationInWebAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(System.Web.Http.HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+
+    }
+}
